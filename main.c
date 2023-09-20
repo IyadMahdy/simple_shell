2f33c0270aa92ce54727a25761da51bf81d2e0d7
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-09-20 06:32:44 -0700

    Task 1

--- main.c
+++ main.c
@@ -12,10 +12,10 @@ int main(int argc, char **argv, char **env)
 {
 	char *input_str = NULL;
 	size_t n = 0;
-	char *args[2];
-	pid_t pid;
 
 	(void) argc;
+	(void) argv;
+	(void) env;
 	while (1)
 	{
 		printf("(SHELL)$ ");
@@ -23,20 +23,7 @@ int main(int argc, char **argv, char **env)
 			break;
 		input_str = strtok(input_str, "\n");
 		if (access(input_str, X_OK) == -1)
-		{
-			fprintf(stderr, "%s: No such file or directory\n", argv[0]);
-			continue;
-		}
-		args[0] = input_str;
-		args[1] = NULL;
-		pid = fork();
-                if (pid == 0)
-                {
-                        if (execve(input_str, args, env) == -1)
-                                perror("execve");
-                }
-                else
-                        wait(NULL);
+			fprintf(stderr, "No such file or directory\n");
 	}
 	free(input_str);
 	return (0);
